9d011bef4b07d53842471beb85368a0c1e1cafbd
abinQAQ11 <1416288754@qq.com>
2025-04-27 09:29:40 +0800

    Delete RingToolkit/Track.py

--- RingToolkit/Track.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-from .Write import ele_setup
-# ----------------------------------------------------------------------------------------------------------------------
-def track(lattice):
-    ele_setup(lattice)
-    subprocess.run(f"elegant {lattice.name}.ele")
